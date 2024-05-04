85c874a182d386f58ed034f0c211b4b96c7b0b6b
KONJA KUMA SAMPSON <nkumasampson@gmail.com>
2024-05-04 08:18:40 -0700

    4th May update

--- src/helpers/counterFormat.js
+++ src/helpers/counterFormat.js
@@ -3,5 +3,6 @@ export const countValueFormat = (value) => {
     if (value >= 1000 && value < 1000000) return `${((value) / 1000).toFixed(1)}k`;
     if (value >= 1000000 && value < 1000000000) return `${((value) / 1000000).
         toFixed(1)}M`;
-    if (value >= 1000000000) return `${((value) / 1000000000).toFixed(1)}T`;
+    if (value >= 1000000000 && value < 1000000000000) return `${((value) / 1000000000).toFixed(1)}B`;
+    if (value >= 1000000000000) return `${((value) / 1000000000000).toFixed(1)}T`;
 }
