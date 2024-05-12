bf36f4c986ccd046e7b4bdb23bd8dced980db2d8
KONJA KUMA SAMPSON <nkumasampson@gmail.com>
2024-05-12 14:22:19 -0700

    new updates

--- src/stores/commentStore.js
+++ src/stores/commentStore.js
@@ -16,6 +16,7 @@ export const useCommentStore = defineStore('commentStore', {
              */
             let comments = JSON.parse(localStorage.getItem("comments"));
             let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+            localStorage.clear();
             if ((!comments) && (!currentUser)) {
                 try {
                     this.loading = true;
@@ -23,6 +24,7 @@ export const useCommentStore = defineStore('commentStore', {
                     this.comments = res.comments;
                     this.currentUser = res.currentUser;
                     this.loading = false;
+                    console.log("loaded from here")
                 } catch (error) {
                     console.log(error);
                 }
